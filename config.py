75aa164e7d55f67ffa3737e5317dc1841ba718e0
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2024-02-05 14:53:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ BOT_TOKEN = 'MTIwMTQ4MzIxNDcyOTAwNzE0NA.GrBIA0.2to-Dy7iSR7OoRBHm0yuUfcF1pOlnccl3
 YOUTUBE_API_KEY = 'AIzaSyDfEis1F2ZFGsOssyzTXXbSY6q5dhk_ydw'
 BOT_PREFIX = '+'
 EMBEDCOLOR = '0xF8AA2A'
-# BOT_ACTIVITY_TYPE = watching
+BOT_ACTIVITY_TYPE = '2' # 0: None, 1: Playing, 2: Listening, 3: Watching, 4: Streaming, 5: Competing
 BOT_ACTIVITY_NAME = '+help'
